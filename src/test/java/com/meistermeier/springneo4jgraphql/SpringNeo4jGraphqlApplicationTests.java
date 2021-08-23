1bb6e65f16436fdb46a35d4fa263ae2c8129eadf
Gerrit Meier <meistermeier@gmail.com>
2021-08-23 16:44:05 +0200

    Add (disabled) test.

--- src/test/java/com/meistermeier/springneo4jgraphql/SpringNeo4jGraphqlApplicationTests.java
+++ src/test/java/com/meistermeier/springneo4jgraphql/SpringNeo4jGraphqlApplicationTests.java
@@ -1,13 +1,34 @@
 package com.meistermeier.springneo4jgraphql;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.graphql.boot.test.tester.AutoConfigureGraphQlTester;
+import org.springframework.graphql.test.tester.GraphQlTester;
 
 @SpringBootTest
-class SpringNeo4jGraphqlApplicationTests {
+@AutoConfigureMockMvc
+@AutoConfigureGraphQlTester
+@Disabled("The GraphQL Tester bean does not get instantiated :( :shrug:")
+public class SpringNeo4jGraphqlApplicationTests {
+
+    @Autowired
+    private GraphQlTester graphQlTester;
 
     @Test
-    void contextLoads() {
+    void resultMatchesExpectation() {
+        String query = "{" +
+                "  movie(title:\"The Matrix\") {" +
+                "    title" +
+                "  }" +
+                "}";
+
+        this.graphQlTester.query(query)
+                .execute()
+                .path("movie")
+                .matchesJson("{\"title\":\"The Matrix\"}");
     }
 
 }
