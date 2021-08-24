732eeb44aa00a63e3388c0aca901aec9247fc1ae
Gerrit Meier <meistermeier@gmail.com>
2021-08-24 09:55:58 +0200

    Enable tests.

--- src/test/java/com/meistermeier/springneo4jgraphql/SpringNeo4jGraphqlApplicationTests.java
+++ src/test/java/com/meistermeier/springneo4jgraphql/SpringNeo4jGraphqlApplicationTests.java
@@ -1,21 +1,40 @@
 package com.meistermeier.springneo4jgraphql;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.graphql.boot.test.tester.AutoConfigureGraphQlTester;
-import org.springframework.graphql.test.tester.GraphQlTester;
+import org.springframework.graphql.test.tester.WebGraphQlTester;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.Neo4jContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureGraphQlTester
-@Disabled("The GraphQL Tester bean does not get instantiated :( :shrug:")
+@Testcontainers
 public class SpringNeo4jGraphqlApplicationTests {
 
+    @Container
+    private static Neo4jContainer<?> neo4jContainer = new Neo4jContainer<>("neo4j:4.3").withoutAuthentication();
+    private final WebGraphQlTester graphQlTester;
+
     @Autowired
-    private GraphQlTester graphQlTester;
+    public SpringNeo4jGraphqlApplicationTests(WebGraphQlTester graphQlTester) {
+        this.graphQlTester = graphQlTester;
+    }
+
+    @DynamicPropertySource
+    static void neo4jProperties(DynamicPropertyRegistry registry) {
+
+        registry.add("spring.neo4j.uri", neo4jContainer::getBoltUrl);
+        registry.add("spring.neo4j.authentication.username", () -> "neo4j");
+        registry.add("spring.neo4j.authentication.password", () -> null);
+        registry.add("tmdb_api", () -> "n/a");
+    }
 
     @Test
     void resultMatchesExpectation() {
