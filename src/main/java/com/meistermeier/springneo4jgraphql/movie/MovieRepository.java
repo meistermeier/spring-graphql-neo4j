47623e770f0968a92e7c8008bf33b884b3509635
Gerrit Meier <meistermeier@gmail.com>
2021-09-01 18:25:10 +0200

    Upgrade Spring Graphql to 1.0.0-M2.
    Remove wiring and _just_ add `@GraphQlRepository` for magic reasons.

--- src/main/java/com/meistermeier/springneo4jgraphql/movie/MovieRepository.java
+++ src/main/java/com/meistermeier/springneo4jgraphql/movie/MovieRepository.java
@@ -2,6 +2,8 @@ package com.meistermeier.springneo4jgraphql.movie;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.graphql.data.GraphQlRepository;
 
+@GraphQlRepository
 public interface MovieRepository extends Neo4jRepository<Movie, String>, QuerydslPredicateExecutor<Movie> {
 }
