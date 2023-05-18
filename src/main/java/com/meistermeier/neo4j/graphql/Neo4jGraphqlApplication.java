091c0436bba02ab73f918704bf85a5ccfc30251d
Gerrit Meier <meistermeier@gmail.com>
2023-05-18 18:20:42 +0200

    Enable metrics and health.

--- src/main/java/com/meistermeier/neo4j/graphql/Neo4jGraphqlApplication.java
+++ src/main/java/com/meistermeier/neo4j/graphql/Neo4jGraphqlApplication.java
@@ -2,8 +2,10 @@ package com.meistermeier.neo4j.graphql;
 
 import org.neo4j.cypherdsl.core.renderer.Configuration;
 import org.neo4j.cypherdsl.core.renderer.Dialect;
+import org.neo4j.driver.MetricsAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.neo4j.ConfigBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -18,4 +20,11 @@ public class Neo4jGraphqlApplication {
         return Configuration.newConfig().withDialect(Dialect.NEO4J_5).build();
     }
 
+    /**
+     * Makes the Neo4j driver use Micrometer for metrics reporting.
+     */
+    @Bean
+    ConfigBuilderCustomizer configBuilderCustomizer() {
+        return configBuilder -> configBuilder.withMetricsAdapter(MetricsAdapter.MICROMETER);
+    }
 }
